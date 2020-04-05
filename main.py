fa1b694e60547f183afd62b7d9cde42405af626b
mdenko <denkmatt@umich.edu>
2020-04-05 15:49:54 -0700

    Initial committ
    Initial commit (I am really struggling to get this to work).
    
    I first attempted to add a donation via peewee but I kept getting errors such as database is locked.
    
    I then attempted to append the new entries as a dictionary to the already selected data but the data types are different and that is not working.

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import base64
 
 from flask import Flask, render_template, request, redirect, url_for, session
-
-from model import Donation 
+from playhouse.db_url import connect
+from model import Donation, Donor
 
 app = Flask(__name__)
 
@@ -15,9 +15,19 @@ def home():
 def all():
     donations = Donation.select()
     return render_template('donations.jinja2', donations=donations)
-    
+
+@app.route('/create', methods=['GET', 'POST'])
+def create():
+    if request.method == 'POST':
+        db = connect(os.environ.get('DATABASE_URL', 'sqlite:///my_database.db'))
+        db.connect()
+        new_donation = {'name': request.form['name'], 'value':request.form['donation']}
+        donations = dict(Donation.select())
+        donations.append(new_donation)
+        return render_template('donations.jinja2', donations=donations)
+    else:
+        return render_template('create.jinja2')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 6738))
     app.run(host='0.0.0.0', port=port)
-
